c636cfa84f01ab3adbb282834242c4556e162bc4
MoreThrust <peeraphat1997@gmail.com>
2019-08-11 20:21:42 +0700

    1

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ $jsonFlex = [
     ]
   ];
 
-if ( sizeof($request_array['events']) == "แสงสว่าง" ) {
+if ( sizeof($request_array['events']) == 'แสงสว่าง' ) {
     foreach ($request_array['events'] as $event) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
