8304c978ae011526a7d148778dba4ebd9b44e311
MoreThrust <peeraphat1997@gmail.com>
2019-08-12 13:35:17 +0700

    ๅ

--- index.php
+++ index.php
@@ -6,137 +6,140 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
-$jsonFlex = [
-    "type" => "flex",
-    "altText" => "Flex Message",
-    "contents" => [
-        "type" => "bubble",
-        "direction" => "ltr",
-        "header" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "contents" => [
-                [
-                    "type" => "text",
-                    "text" => "Purchase",
-                    "size" => "lg",
-                    "align" => "start",
-                    "weight" => "bold",
-                    "color" => "#009813"
-                ],
-                [
-                    "type" => "text",
-                    "text" => "฿ 100.00",
-                    "size" => "3xl",
-                    "weight" => "bold",
-                    "color" => "#000000"
-                ],
-                [
-                    "type" => "text",
-                    "text" => "Rabbit Line Pay",
-                    "size" => "lg",
-                    "weight" => "bold",
-                    "color" => "#000000"
-                ],
-                [
-                    "type" => "text",
-                    "text" => "2019.02.14 21:47 (GMT+0700)",
-                    "size" => "xs",
-                    "color" => "#B2B2B2"
-                ],
-                [
-                    "type" => "text",
-                    "text" => "Payment complete.",
-                    "margin" => "lg",
-                    "size" => "lg",
-                    "color" => "#000000"
-                ]
-            ]
-        ],
-        "body" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "contents" => [
-                [
-                    "type" => "separator",
-                    "color" => "#C3C3C3"
-                ],
-                [
-                    "type" => "box",
-                    "layout" => "baseline",
-                    "margin" => "lg",
-                    "contents" => [
-                        [
-                            "type" => "text",
-                            "text" => "Merchant",
-                            "align" => "start",
-                            "color" => "#C3C3C3"
-                        ],
-                        [
-                            "type" => "text",
-                            "text" => "BTS 01",
-                            "align" => "end",
-                            "color" => "#000000"
-                        ]
-                    ]
-                ],
-                [
-                    "type" => "box",
-                    "layout" => "baseline",
-                    "margin" => "lg",
-                    "contents" => [
-                        [
-                            "type" => "text",
-                            "text" => "New balance",
-                            "color" => "#C3C3C3"
-                        ],
-                        [
-                            "type" => "text",
-                            "text" => "฿ 45.57",
-                            "align" => "end"
-                        ]
-                    ]
-                ],
-                [
-                    "type" => "separator",
-                    "margin" => "lg",
-                    "color" => "#C3C3C3"
-                ]
-            ]
-        ],
-        "footer" => [
-            "type" => "box",
-            "layout" => "horizontal",
-            "contents" => [
-                [
-                    "type" => "text",
-                    "text" => "View Details",
-                    "size" => "lg",
-                    "align" => "start",
-                    "color" => "#0084B6",
-                    "action" => [
-                        "type" => "uri",
-                        "label" => "View Details",
-                        "uri" => "https://google.co.th/"
-                    ]
-                ]
-            ]
-        ]
-    ]
-];
-
 if (sizeof($request_array['events']) > 0) {
     foreach ($request_array['events'] as $event) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
         $text = $event['message']['text'];
 
+        //========================================== แสงสว่าง ==========================================//
+
         if ($text == 'แสงสว่าง') {
+            $jsonFlex = [
+                "type" => "flex",
+                "altText" => "Flex Message",
+                "contents" => [
+                    "type" => "bubble",
+                    "direction" => "ltr",
+                    "header" => [
+                        "type" => "box",
+                        "layout" => "vertical",
+                        "contents" => [
+                            [
+                                "type" => "text",
+                                "text" => "Purchase",
+                                "size" => "lg",
+                                "align" => "start",
+                                "weight" => "bold",
+                                "color" => "#009813"
+                            ],
+                            [
+                                "type" => "text",
+                                "text" => "฿ 100.00",
+                                "size" => "3xl",
+                                "weight" => "bold",
+                                "color" => "#000000"
+                            ],
+                            [
+                                "type" => "text",
+                                "text" => "Rabbit Line Pay",
+                                "size" => "lg",
+                                "weight" => "bold",
+                                "color" => "#000000"
+                            ],
+                            [
+                                "type" => "text",
+                                "text" => "2019.02.14 21:47 (GMT+0700)",
+                                "size" => "xs",
+                                "color" => "#B2B2B2"
+                            ],
+                            [
+                                "type" => "text",
+                                "text" => "Payment complete.",
+                                "margin" => "lg",
+                                "size" => "lg",
+                                "color" => "#000000"
+                            ]
+                        ]
+                    ],
+                    "body" => [
+                        "type" => "box",
+                        "layout" => "vertical",
+                        "contents" => [
+                            [
+                                "type" => "separator",
+                                "color" => "#C3C3C3"
+                            ],
+                            [
+                                "type" => "box",
+                                "layout" => "baseline",
+                                "margin" => "lg",
+                                "contents" => [
+                                    [
+                                        "type" => "text",
+                                        "text" => "Merchant",
+                                        "align" => "start",
+                                        "color" => "#C3C3C3"
+                                    ],
+                                    [
+                                        "type" => "text",
+                                        "text" => "BTS 01",
+                                        "align" => "end",
+                                        "color" => "#000000"
+                                    ]
+                                ]
+                            ],
+                            [
+                                "type" => "box",
+                                "layout" => "baseline",
+                                "margin" => "lg",
+                                "contents" => [
+                                    [
+                                        "type" => "text",
+                                        "text" => "New balance",
+                                        "color" => "#C3C3C3"
+                                    ],
+                                    [
+                                        "type" => "text",
+                                        "text" => "฿ 45.57",
+                                        "align" => "end"
+                                    ]
+                                ]
+                            ],
+                            [
+                                "type" => "separator",
+                                "margin" => "lg",
+                                "color" => "#C3C3C3"
+                            ]
+                        ]
+                    ],
+                    "footer" => [
+                        "type" => "box",
+                        "layout" => "horizontal",
+                        "contents" => [
+                            [
+                                "type" => "text",
+                                "text" => "View Details",
+                                "size" => "lg",
+                                "align" => "start",
+                                "color" => "#0084B6",
+                                "action" => [
+                                    "type" => "uri",
+                                    "label" => "View Details",
+                                    "uri" => "https://google.co.th/"
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ];
             $data = [
                 'replyToken' => $reply_token,
                 'messages' => [$jsonFlex]
             ];
         }
+
+        //========================================== จบแสงสว่าง ==========================================//
 
         if ($text == 'วิธีใช้') {
             $data = [
