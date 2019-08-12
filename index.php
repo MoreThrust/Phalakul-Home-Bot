61902b420a08dbc99f2b113b72c4d8e6f7146b5e
MoreThrust <peeraphat1997@gmail.com>
2019-08-12 10:23:00 +0700

    ok

--- index.php
+++ index.php
@@ -140,6 +140,7 @@ if ( sizeof($request_array['events']) > 0 ) {
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
         echo "Result: ".$send_result."\r\n";
     }
+    
 }
 
 echo "Last update 12/8/2562";
