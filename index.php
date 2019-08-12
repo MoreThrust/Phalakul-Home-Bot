c99462fc3c00f36b20cfaa35e6a052e34afeccd6
MoreThrust <peeraphat1997@gmail.com>
2019-08-12 13:27:08 +0700

    123

--- index.php
+++ index.php
@@ -130,7 +130,8 @@ if (sizeof($request_array['events']) > 0) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
         $text = $event['message']['text'] == 'แสงสว่าง';
-        $data = [
+        if($text == true){
+            $data = [
             'replyToken' => $reply_token,
             // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
             'messages' => [['type' => 'text', 'text' => $text]]
@@ -138,6 +139,8 @@ if (sizeof($request_array['events']) > 0) {
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
         echo "Result: " . $send_result . "\r\n";
+        }
+        
     }
 }
 
