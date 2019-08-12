5b472921595768ccc57c536ac34313b4978ab5d9
MoreThrust <peeraphat1997@gmail.com>
2019-08-12 12:59:35 +0700

    1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 $message = $request_array->{"events"}[0]->{"message"};
 
-echo $request_array;
+echo $request_array/n;
 
 $jsonFlex = [
     "type" => "flex",
