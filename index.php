8ff37d94bf93486331080f4e5c17b8a2002876d5
MoreThrust <peeraphat1997@gmail.com>
2019-08-12 13:52:12 +0700

    123

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include("template.php");
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = '0o0ysAsM9n8ogghDWIpHI7Hes2cIhXyaES1Hu5QA1GoJFpoF3/oR3QHHO8CJ41G1XzU2HLh7fYPXFNfUF5vHnTMHfzvVvV5bmjY8sYQAMEr6dt3JE6A0Oz8Iync6wKjk+VO3OvdvsQSOu6TSu8liQwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'af6aadbace7145526e4d024d7b605bb5';
@@ -14,125 +15,7 @@ if (sizeof($request_array['events']) > 0) {
 
         //========================================== แสงสว่าง ==========================================//
 
-        if ($text == 'แสงสว่าง') {
-            $jsonFlex = [
-                "type" => "flex",
-                "altText" => "Flex Message",
-                "contents" => [
-                    "type" => "bubble",
-                    "direction" => "ltr",
-                    "header" => [
-                        "type" => "box",
-                        "layout" => "vertical",
-                        "contents" => [
-                            [
-                                "type" => "text",
-                                "text" => "Purchase",
-                                "size" => "lg",
-                                "align" => "start",
-                                "weight" => "bold",
-                                "color" => "#009813"
-                            ],
-                            [
-                                "type" => "text",
-                                "text" => "฿ 1000.00",
-                                "size" => "3xl",
-                                "weight" => "bold",
-                                "color" => "#000000"
-                            ],
-                            [
-                                "type" => "text",
-                                "text" => "Rabbit Line Pay",
-                                "size" => "lg",
-                                "weight" => "bold",
-                                "color" => "#000000"
-                            ],
-                            [
-                                "type" => "text",
-                                "text" => "2019.02.14 21:47 (GMT+0700)",
-                                "size" => "xs",
-                                "color" => "#B2B2B2"
-                            ],
-                            [
-                                "type" => "text",
-                                "text" => "Payment complete.",
-                                "margin" => "lg",
-                                "size" => "lg",
-                                "color" => "#000000"
-                            ]
-                        ]
-                    ],
-                    "body" => [
-                        "type" => "box",
-                        "layout" => "vertical",
-                        "contents" => [
-                            [
-                                "type" => "separator",
-                                "color" => "#C3C3C3"
-                            ],
-                            [
-                                "type" => "box",
-                                "layout" => "baseline",
-                                "margin" => "lg",
-                                "contents" => [
-                                    [
-                                        "type" => "text",
-                                        "text" => "Merchant",
-                                        "align" => "start",
-                                        "color" => "#C3C3C3"
-                                    ],
-                                    [
-                                        "type" => "text",
-                                        "text" => "BTS 01",
-                                        "align" => "end",
-                                        "color" => "#000000"
-                                    ]
-                                ]
-                            ],
-                            [
-                                "type" => "box",
-                                "layout" => "baseline",
-                                "margin" => "lg",
-                                "contents" => [
-                                    [
-                                        "type" => "text",
-                                        "text" => "New balance",
-                                        "color" => "#C3C3C3"
-                                    ],
-                                    [
-                                        "type" => "text",
-                                        "text" => "฿ 45.57",
-                                        "align" => "end"
-                                    ]
-                                ]
-                            ],
-                            [
-                                "type" => "separator",
-                                "margin" => "lg",
-                                "color" => "#C3C3C3"
-                            ]
-                        ]
-                    ],
-                    "footer" => [
-                        "type" => "box",
-                        "layout" => "horizontal",
-                        "contents" => [
-                            [
-                                "type" => "text",
-                                "text" => "View Details",
-                                "size" => "lg",
-                                "align" => "start",
-                                "color" => "#0084B6",
-                                "action" => [
-                                    "type" => "uri",
-                                    "label" => "View Details",
-                                    "uri" => "https://google.co.th/"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ];
+        if ($text == 'สถานะ') {
             $data = [
                 'replyToken' => $reply_token,
                 'messages' => [$jsonFlex]
