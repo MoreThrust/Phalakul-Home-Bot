ed9c4912881bb8f6ac623a7e211b72eaee823772
MoreThrust <peeraphat1997@gmail.com>
2019-08-13 14:22:59 +0700

    321

--- index.php
+++ index.php
@@ -44,7 +44,22 @@ if (sizeof($request_array['events']) > 0) {
         }
         //========================================== จบแสงสว่าง ==========================================//
 
-        
+        $imageMapUrl = 'https://www.mywebsite.com/imgsrc/photos/w/sampleimagemap';
+        $replyData = new ImagemapMessageBuilder(
+            $imageMapUrl, // ส่วนของการกำหนด url รูป
+            'This is Imagemap', // ส่วนของการกำหนดหัวเรื่องว่าเกี่ยวกับอะไร
+            new BaseSizeBuilder(699, 1040), // กำหนดขนาดของรูป (สูง,กว้าง)
+            array(
+                new ImagemapMessageActionBuilder(
+                    'test image map',
+                    new AreaBuilder(0, 0, 520, 699)
+                ),
+                new ImagemapUriActionBuilder(
+                    'http://www.ninenik.com',
+                    new AreaBuilder(520, 0, 520, 699)
+                )
+            )
+        );
 
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
         $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
