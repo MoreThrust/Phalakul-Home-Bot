a5142f3918196649074448db9466032e0cb14825
MoreThrust <peeraphat1997@gmail.com>
2019-08-15 00:02:32 +0700

    123

--- Status.php
+++ Status.php
@@ -1,9 +1,29 @@
+<!-- ========================================  STATUS ======================================== -->
+
+
+
 <!-- ==================== Voltage ==================== -->
 <?php $curl = curl_init(); 
 curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => 1, 
 CURLOPT_URL => 'https://api.anto.io/channel/get/DL32Cs80BDqJwgimtX5oBIDTMOlAt8VUBPGElAA4/R_H_A_S/Voltage', CURLOPT_USERAGENT => 'Codular Sample cURL Request' )); 
 $resp = curl_exec($curl); curl_close($curl); $val = explode('"', $resp); $st_Voltage = $val[7]."V"; ?>
 <!-- ==================== END Voltage ==================== -->
+
+
+<!-- ==================== Temp ==================== -->
+<?php $curl = curl_init(); 
+curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => 1, 
+CURLOPT_URL => 'https://api.anto.io/channel/get/DL32Cs80BDqJwgimtX5oBIDTMOlAt8VUBPGElAA4/R_H_A_S/living_temp', CURLOPT_USERAGENT => 'Codular Sample cURL Request' )); 
+$resp = curl_exec($curl); curl_close($curl); $val = explode('"', $resp); $st_living_temp = $val[7]."°C"; ?>
+<!-- ==================== END Temp ==================== -->
+
+
+<!-- ==================== HUMI ==================== -->
+<?php $curl = curl_init(); 
+curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => 1, 
+CURLOPT_URL => 'https://api.anto.io/channel/get/DL32Cs80BDqJwgimtX5oBIDTMOlAt8VUBPGElAA4/R_H_A_S/living_humi', CURLOPT_USERAGENT => 'Codular Sample cURL Request' )); 
+$resp = curl_exec($curl); curl_close($curl); $val = explode('"', $resp); $st_living_humi = $val[7]."%"; ?>
+<!-- ==================== END HUMI ==================== -->
 
 
 <!-- ==================== Rain ==================== -->
@@ -16,207 +36,27 @@ if($val[7] == "1"){$st_rain = "ฝนกำลังตก";}elseif($val[7] == "
 ?>
 <!-- ==================== END Rain ==================== -->
 
+
+<!-- ==================== Door ==================== -->
 <?php
 $curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/lamp_meeting_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_lamp_mt = "ไฟห้องประชุม: เปิดอยู่";
-    $st_lamp_mt2 = " 􀔃􀇣blue circle􏿿";
-}elseif($val[7] == "0"){
-    $st_lamp_mt = "ไฟห้องประชุม: ปิดอยู่";
-    $st_lamp_mt2 = " 􀔃􀇢red circle􏿿";
-} 
+curl_setopt_array($curl, array(CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_URL => 'https://api.anto.io/channel/get/DL32Cs80BDqJwgimtX5oBIDTMOlAt8VUBPGElAA4/R_H_A_S/Door',CURLOPT_USERAGENT => 'Codular Sample cURL Request'));
+$resp = curl_exec($curl);curl_close($curl);$val = explode('"', $resp);
+if($val[7] == "1"){$st_door = "ล็อกแล้ว";}elseif($val[7] == "0"){$st_door = "ยังไม่ได้ล็อก";}
 ?>
+<!-- ==================== END Door ==================== -->
 
+<!-- ==================== Lamp ==================== -->
 <?php
 $curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/lamp_reception_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_lamp_rt = "ไฟห้องรับแขก: เปิดอยู่";
-    $st_lamp_rt2 = " 􀔃􀇣blue circle􏿿";
-}elseif($val[7] == "0"){
-    $st_lamp_rt = "ไฟห้องรับแขก: ปิดอยู่";
-    $st_lamp_rt2 = " 􀔃􀇢red circle􏿿";
-}   
+curl_setopt_array($curl, array(CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_URL => 'https://api.anto.io/channel/get/DL32Cs80BDqJwgimtX5oBIDTMOlAt8VUBPGElAA4/R_H_A_S/living_lamp',CURLOPT_USERAGENT => 'Codular Sample cURL Request'));
+$resp = curl_exec($curl);curl_close($curl);$val = explode('"', $resp);
+if($val[7] == "1"){$st_living_lamp = "เปิดอยู่";}elseif($val[7] == "0"){$st_living_lamp = "ปิดอยู่";}
 ?>
+<!-- ==================== END Lamp ==================== -->
 
-<!-- ==================== End Status Lamp ==================== -->
 
-<!-- ==================== Status Air ==================== -->
 
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/air_workshop_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_air_ws = "แอร์ห้องทำงาน: เปิดอยู่";
-    $st_air_ws2 = " 􀔃􀇣blue circle􏿿";
-}elseif($val[7] == "0"){
-    $st_air_ws = "แอร์ห้องทำงาน: ปิดอยู่";
-    $st_air_ws2 = " 􀔃􀇢red circle􏿿";
-}
-?>
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/air_meeting_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_air_mt = "แอร์ห้องประชุม: เปิดอยู่";
-    $st_air_mt2 = " 􀔃􀇣blue circle􏿿";
-}elseif($val[7] == "0"){
-    $st_air_mt = "แอร์ห้องประชุม: ปิดอยู่";
-    $st_air_mt2 = " 􀔃􀇢red circle􏿿";
-}
-?>
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/air_reception_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_air_rt = "แอร์ห้องรับแขก: เปิดอยู่";
-    $st_air_rt2 = " 􀔃􀇣blue circle􏿿";
-}elseif($val[7] == "0"){
-    $st_air_rt = "แอร์ห้องรับแขก: ปิดอยู่";
-    $st_air_rt2 = " 􀔃􀇢red circle􏿿";
-}   
-?>
-
-<!-- ==================== End Status Air ==================== -->
-
-<!-- ==================== Temp Air ==================== -->
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/temp_reception_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-$temp_air_rt = "ห้องรับแขก ".$val[7]."°C";
-?>
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/temp_workshop_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-$temp_air_ws = "ห้องทำงาน ".$val[7]."°C";
-?>
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/temp_meeting_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-$temp_air_mt = "ห้องประชุม ".$val[7]."°C";
-?>
-
-<!-- ==================== End Temp Air ==================== -->
-
-<!-- ==================== Status Door ==================== -->
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/door_entrance',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_door_et = "ทางเข้า: ล็อกอยู่";
-    $st_door_et2 = " 􀔃􀆑lock􏿿";
-}elseif($val[7] == "0"){
-    $st_door_et = "ทางเข้า: ไม่มีการล็อก";
-    $st_door_et2 = " 􀔃􀆒opened lock􏿿";
-}
-?>
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/door_workshop_room',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_door_ws = "ห้องทำงาน: ล็อกอยู่";
-    $st_door_ws2 = " 􀔃􀆑lock􏿿";
-}elseif($val[7] == "0"){
-    $st_door_ws = "ห้องทำงาน: ไม่มีการล็อก";
-    $st_door_ws2 = " 􀔃􀆒opened lock􏿿";
-} 
-?>
-
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.anto.io/channel/get/4GZewdAlDhxWz6ijHnvDSh73Q9rxeOjYNx0SLRgl/Smart_Office/door_meeting_room'
-    /*CURLOPT_USERAGENT => 'Codular Sample cURL Request'*/
-));
-$resp = curl_exec($curl);
-curl_close($curl);
-$val = explode('"', $resp);
-if($val[7] == "1"){
-    $st_door_mt = "ห้องประชุม: ล็อกอยู่";
-    $st_door_mt2 = " 􀔃􀆑lock􏿿";
-}elseif($val[7] == "0"){
-    $st_door_mt = "ห้องประชุม: ไม่มีการล็อก";
-    $st_door_mt2 = " 􀔃􀆒opened lock􏿿";
-}   
-?>
-
-<!-- ==================== End Status Door ==================== -->
+<!-- ======================================== END STATUS ======================================== -->
--- template.php
+++ template.php
@@ -13,157 +13,179 @@ $jsonFlex = ["type" => "flex", "altText" => "Hello Flex Message", "contents" => 
 //$js_ST = [["type" => "flex", "altText" => "Flex Message", "contents" => ["type" => "bubble", "direction" => "ltr", "hero" => ["type" => "image", "url" => "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png", "size" => "full", "aspectRatio" => "20:13", "aspectMode" => "cover"], "body" => ["type" => "box", "layout" => "vertical", "contents" => [["type" => "text", "text" => "สถานะปัจจุบัน", "size" => "xl", "weight" => "bold"], ["type" => "box", "layout" => "horizontal", "spacing" => "xs", "margin" => "lg", "contents" => [["type" => "box", "layout" => "vertical", "flex" => 2, "contents" => [["type" => "text", "text" => "แรงดันไฟฟ้า", "size" => "lg"], ["type" => "text", "text" => "เปลวไฟ", "size" => "lg"], ["type" => "text", "text" => "อุณหภูมิ", "size" => "lg"], ["type" => "text", "text" => "ความชื้น", "size" => "lg"]]], ["type" => "box", "layout" => "vertical", "flex" => 2, "contents" => [["type" => "text", "text" => "223v", "size" => "lg", "weight" => "bold"], ["type" => "text", "text" => "ไม่พบ", "size" => "lg", "weight" => "bold"], ["type" => "text", "text" => "29.7°C", "size" => "lg", "weight" => "bold"], ["type" => "text", "text" => "45%", "size" => "lg", "weight" => "bold"]]]]]]], "footer" => ["type" => "box", "layout" => "vertical", "flex" => 0, "spacing" => "sm", "contents" => [["type" => "separator"], ["type" => "button", "action" => ["type" => "message", "label" => "เปิดแผงควบคุม", "text" => "แผงควบคุม"], "height" => "sm"], ["type" => "separator"], ["type" => "button", "action" => ["type" => "message", "label" => "เช็คสถานะอีกครั้ง", "text" => "สถานะ"], "height" => "sm"], ["type" => "separator"], ["type" => "text", "text" => "เมื่อวันที่ 22/02/62 เวลา 20:30น.", "margin" => "xxl", "size" => "xxs", "align" => "center", "gravity" => "bottom"]]]]]];
 
 $js_ST = [
-    
-  "type" => "flex",
-  "altText" => "Flex Message",
-  "contents" => [
-    "type" => "bubble",
-    "direction" => "ltr",
-    "hero" => [
-      "type" => "image",
-      "url" => "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-      "size" => "full",
-      "aspectRatio" => "20:13",
-      "aspectMode" => "cover"
-    ],
-    "body" => [
-      "type" => "box",
-      "layout" => "vertical",
-      "contents" => [
-        [
-          "type" => "text",
-          "text" => "สถานะปัจจุบัน",
-          "size" => "xl",
-          "weight" => "bold"
+    "type" => "flex",
+    "altText" => "Flex Message",
+    "contents" => [
+        "type" => "bubble",
+        "direction" => "ltr",
+        "hero" => [
+            "type" => "image",
+            "url" => "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+            "size" => "full",
+            "aspectRatio" => "20:13",
+            "aspectMode" => "cover"
         ],
-        [
-          "type" => "box",
-          "layout" => "horizontal",
-          "spacing" => "xs",
-          "margin" => "lg",
-          "contents" => [
-            [
-              "type" => "box",
-              "layout" => "vertical",
-              "flex" => 2,
-              "contents" => [
+        "body" => [
+            "type" => "box",
+            "layout" => "vertical",
+            "contents" => [
                 [
-                  "type" => "text",
-                  "text" => "แรงดันไฟฟ้า",
-                  "size" => "lg"
+                    "type" => "text",
+                    "text" => "สถานะปัจจุบัน",
+                    "size" => "xl",
+                    "weight" => "bold"
                 ],
                 [
-                  "type" => "text",
-                  "text" => "อุณหภูมิ",
-                  "size" => "lg"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "ความชื้น",
-                  "size" => "lg"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "ฝน",
-                  "size" => "lg"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "ประตูบ้าน",
-                  "size" => "lg"
+                    "type" => "box",
+                    "layout" => "vertical",
+                    "spacing" => "sm",
+                    "margin" => "lg",
+                    "contents" => [
+                        [
+                            "type" => "text",
+                            "text" => "ห้องนั่งเล่น",
+                            "size" => "lg",
+                            "weight" => "bold"
+                        ],
+                        [
+                            "type" => "box",
+                            "layout" => "horizontal",
+                            "contents" => [
+                                [
+                                    "type" => "box",
+                                    "layout" => "vertical",
+                                    "flex" => 2,
+                                    "contents" => [
+                                        [
+                                            "type" => "text",
+                                            "text" => "แรงดันไฟฟ้า",
+                                            "size" => "lg"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => "อุณหภูมิ",
+                                            "size" => "lg"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => "ความชื้น",
+                                            "size" => "lg"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => "ฝน",
+                                            "size" => "lg"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => "ประตูบ้าน",
+                                            "size" => "lg"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => "เปิดอยู่",
+                                            "size" => "lg"
+                                        ]
+                                    ]
+                                ],
+                                [
+                                    "type" => "box",
+                                    "layout" => "vertical",
+                                    "flex" => 2,
+                                    "contents" => [
+                                        [
+                                            "type" => "text",
+                                            "text" => $st_Voltage,
+                                            "size" => "lg",
+                                            "weight" => "bold",
+                                            "color" => "#E67E22"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => $st_living_temp,
+                                            "size" => "lg",
+                                            "weight" => "bold",
+                                            "color" => "#3867D6"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => $st_living_humi,
+                                            "size" => "lg",
+                                            "weight" => "bold",
+                                            "color" => "#3867D6"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => $st_rain,
+                                            "size" => "lg",
+                                            "weight" => "bold",
+                                            "color" => "#0FB9B1"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => $st_door,
+                                            "size" => "lg",
+                                            "weight" => "bold",
+                                            "color" => "#EB3B5A"
+                                        ],
+                                        [
+                                            "type" => "text",
+                                            "text" => $st_living_lamp,
+                                            "size" => "lg",
+                                            "weight" => "bold"
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
                 ]
-              ]
-            ],
-            [
-              "type" => "box",
-              "layout" => "vertical",
-              "flex" => 2,
-              "contents" => [
-                [
-                  "type" => "text",
-                  "text" => $st_Voltage,
-                  "size" => "lg",
-                  "weight" => "bold",
-                  "color" => "#E67E22"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "29.7°C",
-                  "size" => "lg",
-                  "weight" => "bold",
-                  "color" => "#3867D6"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "45%",
-                  "size" => "lg",
-                  "weight" => "bold",
-                  "color" => "#3867D6"
-                ],
-                [
-                  "type" => "text",
-                  "text" => $st_rain,
-                  "size" => "lg",
-                  "weight" => "bold",
-                  "color" => "#0FB9B1"
-                ],
-                [
-                  "type" => "text",
-                  "text" => "ยังไม่ได้ล็อก",
-                  "size" => "lg",
-                  "weight" => "bold",
-                  "color" => "#EB3B5A"
-                ]
-              ]
             ]
-          ]
+        ],
+        "footer" => [
+            "type" => "box",
+            "layout" => "vertical",
+            "flex" => 0,
+            "spacing" => "sm",
+            "contents" => [
+                [
+                    "type" => "button",
+                    "action" => [
+                        "type" => "message",
+                        "label" => "เปิดแผงควบคุม",
+                        "text" => "แผงควบคุม"
+                    ],
+                    "color" => "#2980B9",
+                    "height" => "md",
+                    "style" => "primary"
+                ],
+                [
+                    "type" => "button",
+                    "action" => [
+                        "type" => "message",
+                        "label" => "เช็คสถานะอีกครั้ง",
+                        "text" => "สถานะ"
+                    ],
+                    "color" => "#F39C12",
+                    "margin" => "md",
+                    "height" => "md",
+                    "style" => "primary"
+                ],
+                [
+                    "type" => "separator",
+                    "margin" => "xl"
+                ],
+                [
+                    "type" => "text",
+                    "text" => $dt,
+                    "margin" => "lg",
+                    "size" => "xxs",
+                    "align" => "center",
+                    "gravity" => "center"
+                ]
+            ]
         ]
-      ]
-    ],
-    "footer" => [
-      "type" => "box",
-      "layout" => "vertical",
-      "flex" => 0,
-      "spacing" => "sm",
-      "contents" => [
-        [
-          "type" => "button",
-          "action" => [
-            "type" => "message",
-            "label" => "เปิดแผงควบคุม",
-            "text" => "แผงควบคุม"
-          ],
-          "color" => "#2980B9",
-          "height" => "md",
-          "style" => "primary"
-        ],
-        [
-          "type" => "button",
-          "action" => [
-            "type" => "message",
-            "label" => "เช็คสถานะอีกครั้ง",
-            "text" => "สถานะ"
-          ],
-          "color" => "#F39C12",
-          "margin" => "md",
-          "height" => "md",
-          "style" => "primary"
-        ],
-        [
-          "type" => "separator",
-          "margin" => "xl"
-        ],
-        [
-          "type" => "text",
-          "text" => $dt,
-          "margin" => "lg",
-          "size" => "xxs",
-          "align" => "center",
-          "gravity" => "center"
-        ]
-      ]
     ]
-  ]
+
 
 ];
-
