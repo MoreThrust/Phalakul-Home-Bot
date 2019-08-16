67c974d460c8275d79d5349620e3308182f68365
MoreThrust <peeraphat1997@gmail.com>
2019-08-16 19:22:34 +0700

    123

--- template.php
+++ template.php
@@ -18,7 +18,7 @@ $js_wr_login = [
           "text" => "ดูภาพปัจจุบัน"
         ]
       ],
-      "thumbnailImageUrl" => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyt3SDd7nWRPL7Tzc-jQvOpgKkezrmaCMOVm5jUWZajp-f6omreA',
+      "thumbnailImageUrl" => 'http://tv.bectero.com/wp-content/uploads/2018/11/Pg5vQj0D_400x400.jpg',
       "title" => "แจ้งเตือน!!",
       "text" => "คุณ \$name ได้ปลดล็อคประตูบ้าน \n$dt"
     ]
