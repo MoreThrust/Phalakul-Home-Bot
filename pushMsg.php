d10a280cbdef5736408856d2e324285524d90f6f
MoreThrust <peeraphat1997@gmail.com>
2019-08-16 16:26:52 +0700

    123

--- pushMsg.php
+++ pushMsg.php
@@ -27,46 +27,51 @@ $kooID = "Ue124de79c1d8b06ae61ce5bf1039f52f";
 if ($st_Voltage == "230") {
    $arrayPostData['to'] = $kooID;
    $arrayPostData['messages'][0]['type'] = "text";
-   $arrayPostData['messages'][0]['text'] = "ดูเหมือนว่าระดับแรงดันไฟจะเกิน 230 | แรงดันที่วัดได้ = ".$st_Voltage;
+   $arrayPostData['messages'][0]['text'] = "ดูเหมือนว่าระดับแรงดันไฟจะเกิน 230 | แรงดันที่วัดได้ = " . $st_Voltage;
+   pushMsg($arrayHeader, $arrayPostData);
+}
+
+if ($st_Voltage == "230") {
+   $arrayPostData['to'] = $kooID;
+   $arrayPostData['messages'][0]['type'] = "text";
+   $arrayPostData['messages'][0]['text'] = "ดูเหมือนว่าระดับแรงดันไฟจะเกิน 230 | แรงดันที่วัดได้ = " . $st_Voltage;
    pushMsg($arrayHeader, $arrayPostData);
 }
 
 if ($st_Voltage == "232") {
    $arrayPostData['to'] = $kooID;
-   $arrayPostData['messages'][0]['type'] = "text";
-   $arrayPostData['messages'][0]['text'] = "ดูเหมือนว่าระดับแรงดันไฟจะเกิน 232 | แรงดันที่วัดได้ = ".$st_Voltage;
-   $data = [
-      'replyToken' => $reply_token,
-      'messages' => ["ดูเหมือนว่าระดับแรงดันไฟจะเกิน 232 | แรงดันที่วัดได้ = ".$st_Voltage]
-   ];
+   $arrayPostData['messages'][0]['type'] =
+      [
+         'replyToken' => $reply_token,
+         'messages' => ["ดูเหมือนว่าระดับแรงดันไฟจะเกิน 234 | แรงดันที่วัดได้ = " . $st_Voltage]
+      ];
    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
    pushMsg($POST_HEADER, $post_body);
 }
 
 if ($st_Voltage >= "234") {
-      $data = [
-         'replyToken' => $reply_token,
-         'messages' => ["ดูเหมือนว่าระดับแรงดันไฟจะเกิน 234 | แรงดันที่วัดได้ = ".$st_Voltage]
-      ];
-      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-      $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
-      echo "Result: " . $send_result . "\r\n";
-      pushMsg($POST_HEADER, $post_body);
-   
+   $data = [
+      'replyToken' => $reply_token,
+      'messages' => ["ดูเหมือนว่าระดับแรงดันไฟจะเกิน 234 | แรงดันที่วัดได้ = " . $st_Voltage]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+   $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
+   echo "Result: " . $send_result . "\r\n";
+   pushMsg($POST_HEADER, $post_body);
 }
 
 
 function send_reply_message($url, $post_header, $post_body)
 {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
+   $ch = curl_init($url);
+   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   $result = curl_exec($ch);
+   curl_close($ch);
+   return $result;
 }
 
 function pushMsg($arrayHeader, $arrayPostData)
