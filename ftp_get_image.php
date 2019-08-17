ba6860ce629e488cfabc81cc9f142a0657e4ded6
MoreThrust <peeraphat1997@gmail.com>
2019-08-17 15:12:11 +0700

    321

--- ftp_get_image.php
+++ ftp_get_image.php
@@ -2,5 +2,5 @@
 
 header('Content-Type: image/jpeg');
 echo "ok";
-echo file_get_contents('ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg');
+//echo file_get_contents('ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg');
 //ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg
