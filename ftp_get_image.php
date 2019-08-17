ccc8496ee8bc1c36fd4836f6419c4a272e5765f2
MoreThrust <peeraphat1997@gmail.com>
2019-08-17 15:28:04 +0700

    321

--- ftp_get_image.php
+++ ftp_get_image.php
@@ -1,4 +1,12 @@
+<?
+
+header('Content-Type: image/jpeg');
+
+//echo file_get_contents('ftp://username:password@ftp.example.com/path/image.jpg');
+echo file_get_contents('ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg');
+//ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg
+?>
+<a src="image.php" /><br>
 <img src='ftp://cam:more160340@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg' alt='my picture'>
 
-//echo file_get_contents('ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg');
-//ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg
+
