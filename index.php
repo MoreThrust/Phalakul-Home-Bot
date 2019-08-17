dab4d70ae91e3962ddb1997831acf1292686e0b8
MoreThrust <peeraphat1997@gmail.com>
2019-08-17 22:37:42 +0700

    123

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 </html>
 <?php
 include("template.php");
+include("ftp_get_image.php");
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = '0o0ysAsM9n8ogghDWIpHI7Hes2cIhXyaES1Hu5QA1GoJFpoF3/oR3QHHO8CJ41G1XzU2HLh7fYPXFNfUF5vHnTMHfzvVvV5bmjY8sYQAMEr6dt3JE6A0Oz8Iync6wKjk+VO3OvdvsQSOu6TSu8liQwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'af6aadbace7145526e4d024d7b605bb5';
