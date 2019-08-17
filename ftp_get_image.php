aebbd32f35a5945a9ec909b031f2dcf66151f76a
MoreThrust <peeraphat1997@gmail.com>
2019-08-17 15:35:02 +0700

    123

--- ftp_get_image.php
+++ ftp_get_image.php
@@ -1,26 +1,3 @@
 <?php
-$content = '';
-$ftp_server = "192.168.1.48:224";
-$ftp_user = "cam";
-$ftp_pass = "more";
-$file_name = "/CAM/UDON_CAM/20140808/2014080801150401.jpg";
-$conn_id = ftp_connect($ftp_server) or die("Couldn't connect to $ftp_server"); 
-
-if (@ftp_login($conn_id, $ftp_user, $ftp_pass)) {
-    $content .= "<br />Connected as $ftp_user@$ftp_server\n";
-} else {
-    $content .= "<br />Couldn't connect as $ftp_user\n";
-}
-
-$files = ftp_nlist($conn_id, $dir);
-foreach($files as $file_name)
-{
-    $content.=  '
-         <div>
-            <a href="ftp://'.$ftp_server.'/'.$file_name.'">
-            <img src="ftp://'.$ftp_server.'/'.$file_name.'"    width="150" height="150">
-           </a>
-         </div>';
-}
 //ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg
 ?>
