b810f61a0f64d0c4ab052cb8734d42546939800d
MoreThrust <peeraphat1997@gmail.com>
2019-08-18 15:23:18 +0700

    123

--- ftp_get_image.php
+++ ftp_get_image.php
@@ -1,6 +1,19 @@
 <?php
-echo "v2";
+echo "v3";
 
+$conn = ftp_connect("morethrust.synology.me") or die("Could not connect");
+$res = ftp_login($conn,"cam","more160340");
+
+if($res)
+{
+    echo "Success FTP is Connecting";
+}
+else
+{
+    echo "FTP don't connect";
+}
+
+/*
 // FTP server details
 $ftpHost   = 'ftp.morethrust.synology.me:224';
 $ftpUsername = 'cam';
@@ -18,7 +31,7 @@ if(@ftp_login($connId, $ftpUsername, $ftpPassword)){
 
 // close the connection
 ftp_close($connId);
-
+*/
 
 //ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg
 //ftp://115.87.177.18:224/CAM/UDON_CAM/
