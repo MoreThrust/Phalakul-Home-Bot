b82696271a6e8b251eb28741d8d909f72ded4097
MoreThrust <peeraphat1997@gmail.com>
2019-08-18 13:16:03 +0700

    123

--- ftp_get_image.php
+++ ftp_get_image.php
@@ -3,7 +3,7 @@ echo "start 5";
 
 header('Content-Type: image/jpeg');
 
-echo file_get_contents('http://cam:more160340@115.87.177.18:224/CAM/UDON_CAM/20140808/2014080801150401.jpg');
+echo file_get_contents('http://cam:more160340@ftp://morethrust.synology.me:224/CAM/UDON_CAM/20140808/2014080801150401.jpg');
 
 //ftp://cam@192.168.1.48:224/CAM/UDON_CAM/20140808/2014080801150401.jpg
 //ftp://115.87.177.18:224/CAM/UDON_CAM/
