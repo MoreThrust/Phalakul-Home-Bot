d3e45e0df670d14409905940fcef4ba4694f30e0
MoreThrust <peeraphat1997@gmail.com>
2019-08-20 18:36:24 +0700

    123

--- Arduino_code/RHAS-Project/RHAS-Project.ino
+++ Arduino_code/RHAS-Project/RHAS-Project.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <AntoIO.h>
 
+const char *ssid = "your access point SSID";
+const char *pass = "access point password";
+const char *user = "your username";
+const char *token = "your token";
+const char *thing = "your thing";
+
+AntoIO anto(user, token, thing);
+
+void setup() {
+  Serial.begin(115200);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
