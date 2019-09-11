a1ad33a3ce0aebd23afce867d3a4baa43edb9305
MoreThrust <peeraphat1997@gmail.com>
2019-09-12 00:06:37 +0700

    123

--- ai.php
+++ ai.php
@@ -1,14 +1,32 @@
-<?php 
-$curl = curl_init(); 
-curl_setopt_array($curl, array( 
-    CURLOPT_RETURNTRANSFER => 1, 
-    CURLOPT_URL => 'https://api.anto.io/channel/get/DL32Cs80BDqJwgimtX5oBIDTMOlAt8VUBPGElAA4/R_H_A_S/Voltage', 
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request' 
-)); 
-
-$resp = curl_exec($curl); curl_close($curl); 
-echo $resp; 
-$val = explode('"', $resp); 
-$st_Voltage = $val[7]; 
-
+<?php
+ 
+$curl = curl_init();
+$img_file = ("http://static.weloveshopping.com/shop/client/000032/weloveplate/replate002.jpg");
+$data = array("image" => new CURLFile($img_file, mime_content_type($img_file), basename($img_file)));
+ 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.aiforthai.in.th/lpr",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => $data,
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: multipart/form-data",
+    "apikey: MWj32O4MsQUohPtEbTSqGcPr9UPX1B6I"
+  )
+));
+ 
+$response = curl_exec($curl);
+$err = curl_error($curl);
+ 
+curl_close($curl);
+ 
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
 ?>
