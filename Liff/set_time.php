ed71c782d8d90a636af92efb3a8ebc642cb429ec
MoreThrust <peeraphat1997@gmail.com>
2019-09-23 18:02:37 +0700

    123

--- Liff/set_time.php
+++ Liff/set_time.php
@@ -17,13 +17,7 @@
         };
 
         function initializeApp(data) {
-            document.getElementById('languagefield').textContent = data.language;
-            document.getElementById('viewtypefield').textContent = data.context.viewType;
-            document.getElementById('useridfield').textContent = data.context.userId;
-            document.getElementById('utouidfield').textContent = data.context.utouId;
-            document.getElementById('roomidfield').textContent = data.context.roomId;
-            document.getElementById('groupidfield').textContent = data.context.groupId;
-
+            
             // closeWindow call
             document.getElementById('closewindowbutton').addEventListener('click', function() {
                 liff.closeWindow();
