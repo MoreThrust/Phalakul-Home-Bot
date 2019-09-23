4911c3dfcb984b9b9ea6d105dfe6b40400665912
MoreThrust <peeraphat1997@gmail.com>
2019-09-23 21:32:45 +0700

    123

--- template.php
+++ template.php
@@ -13,13 +13,6 @@ $js_status = [
     "contents" => [
       "type" => "bubble",
       "direction" => "ltr",
-      "hero" => [
-        "type" => "image",
-        "url" => "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-        "size" => "full",
-        "aspectRatio" => "20:13",
-        "aspectMode" => "cover"
-      ],
       "body" => [
         "type" => "box",
         "layout" => "vertical",
