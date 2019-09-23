a19321c8d07fe508166f74fde6affeb7578634fd
MoreThrust <peeraphat1997@gmail.com>
2019-09-23 17:40:20 +0700

    123

--- Liff/set_time.php
+++ Liff/set_time.php
@@ -30,23 +30,6 @@
             });
 
         }
-
-        function toggleAccessToken() {
-            toggleElement('accesstokendata');
-        }
-
-        function toggleProfileData() {
-            toggleElement('profileinfo');
-        }
-
-        function toggleElement(elementId) {
-            const elem = document.getElementById(elementId);
-            if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-                elem.style.display = "none";
-            } else {
-                elem.style.display = "block";
-            }
-        }
     </script>
 
     <div class="buttongroup">
