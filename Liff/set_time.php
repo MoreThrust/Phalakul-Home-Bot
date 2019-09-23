69d0acc9fe34de4d1887eea891ec24818c417292
MoreThrust <peeraphat1997@gmail.com>
2019-09-23 18:07:22 +0700

    123

--- Liff/set_time.php
+++ Liff/set_time.php
@@ -2,36 +2,6 @@
 <html lang="en">
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LIFF Starter</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-
-    <script>
-        window.onload = function(e) {
-            liff.init(function(data) {
-                initializeApp(data);
-            });
-        };
-
-        function initializeApp(data) {
-            document.getElementById('closewindowbutton').addEventListener('click', function() {
-                liff.closeWindow();
-            });
-        }
-    </script>
-
-        <button id="closewindowbutton">บันทึก</button>
-
-        <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
-        <!--<script src="liff-starter.js"></script>-->
-</body>
-
-<!--
-
-<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -48,7 +18,7 @@
             <div class="col-sm"></div>
             <div class="col-sm">
 
-                <?php /* $curl = curl_init();
+                <?php  $curl = curl_init();
                 curl_setopt_array($curl, array(
                     CURLOPT_RETURNTRANSFER => 1,
                     CURLOPT_URL => 'https://api.anto.io/channel/get/DL32Cs80BDqJwgimtX5oBIDTMOlAt8VUBPGElAA4/R_H_A_S/living_lamp_time_on', CURLOPT_USERAGENT => 'Codular Sample cURL Request'
@@ -60,10 +30,8 @@
 
                 ?>
 
-                เวลาเปิดที่ตั้งไว้ <?php /* echo $st_ton; ?> น.
+                เวลาเปิดที่ตั้งไว้ <?php  echo $st_ton; ?> น.
 
-
-                <button id="closewindowbutton">Close</button>
                 <button type="button" id="closewindowbutton" >บันทึกเวลา</div>
 
             </div>
@@ -75,7 +43,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>-->
+</body>
 
 </html>
 
