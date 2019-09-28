6d9bce6acf808b3cd787d32d2d5c97f815b974a6
MoreThrust <peeraphat1997@gmail.com>
2019-09-28 11:15:17 +0700

    Update pushMsg.php

--- pushMsg.php
+++ pushMsg.php
@@ -2,21 +2,19 @@
 include("template.php");
 include("Status.php");
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = '0o0ysAsM9n8ogghDWIpHI7Hes2cIhXyaES1Hu5QA1GoJFpoF3/oR3QHHO8CJ41G1XzU2HLh7fYPXFNfUF5vHnTMHfzvVvV5bmjY8sYQAMEr6dt3JE6A0Oz8Iync6wKjk+VO3OvdvsQSOu6TSu8liQwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'af6aadbace7145526e4d024d7b605bb5';
+$ACCESS_TOKEN = 'DbDJI21eID/MzvRJKEE97bA+5F2zQrx/V/JRC0N+hUKmOIsBLAc5kTMdw0lJgutrFYhZ7jj8HuTB4YMoPWvnb3P8n6Lbf67YdXiUXJDm1kAiOIVXMo1NXaUQ1kiKv8wxktZH1Sd6CWhUi0QxjhM9+AdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'a5975c29114c71eca67869c2933db9bd';
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
 $kooID = "Ue124de79c1d8b06ae61ce5bf1039f52f";
 
-if($st_door == 'ยังไม่ได้ล็อก') {
    $data = [
        'to' => $kooID,
-       'messages' => [$js_q_replay]
+       'messages' => [$js_cam]
    ];
    pushMsg2($POST_HEADER, $data);
-}
 /*
 if($st_living_humi == '57.3') {
    $data = [
